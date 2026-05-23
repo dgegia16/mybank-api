8a1787b20116f2b766a8ddc679eb7fe8a1232003
daati35 <125276422+dgegia16@users.noreply.github.com>
2026-05-23 12:55:00 +0400

    added transactions endpoints

--- app/main.py
+++ app/main.py
@@ -20,7 +20,7 @@ def root():
     return {"message": "Welcome to MyBank API"}
 
 
-@app.post("/accounts")
+@app.post("/accounts", tags=["Accounts"])
 def create_account(owner: str, balance: float = 0.0, db: Session = Depends(get_db)):
     account = Account(owner=owner, balance=balance)
     db.add(account)
@@ -28,10 +28,61 @@ def create_account(owner: str, balance: float = 0.0, db: Session = Depends(get_d
     db.refresh(account)
     return {"id": account.id, "owner": account.owner, "balance": account.balance}
 
-@app.get("/accounts/{account_id}")
+@app.get("/accounts/{account_id}", tags=["Accounts"])
 def get_accounts(account_id: int, db: Session = Depends(get_db)):
     account = db.query(Account).filter(Account.id == account_id).first()
     if not account:
         return {"error": "Account not found"}
 
     return {"id": account.id, "owner": account.owner, "balance": account.balance}
+
+@app.post("/accounts/{account_id}/deposit", tags=["Transactions"])
+def deposit_money(account_id: int, amount: float, db: Session = Depends(get_db)):
+    account = db.query(Account).filter(Account.id == account_id).first()
+
+    if not account:
+        return {"error": "Account not found"}
+
+    if amount <= 0:
+        return {"error": "Amount must be positive"}
+    
+    account.balance += amount
+    db.commit()
+    return {"id": account.id, "owner": account.owner, "balance": account.balance}
+
+@app.post("/accounts/{account_id}/withdraw", tags=["Transactions"])
+def withdraw_money(account_id: int, amount: float, db: Session = Depends(get_db)):
+    account = db.query(Account).filter(Account.id == account_id).first()
+
+    if not account:
+        return {"error": "Account not found"}
+
+    if amount <= 0:
+        return {"error": "Amount must be positive"}
+    
+    if amount > account.balance:
+        return {"error": "Not enough balance"}
+
+    account.balance -= amount
+    db.commit()
+    return {"id": account.id, "owner": account.owner, "balance": account.balance}
+
+
+@app.post("/accounts/transfer", tags=["Transactions"])
+def transfer_to_someone(from_id: int, to_id: int, amount: float, db: Session = Depends(get_db)):
+    sender = db.query(Account).filter(Account.id == from_id).first()
+    receiver = db.query(Account).filter(Account.id == to_id).first()
+
+    if amount <= 0:
+        return {"error": "Amount must be positive"}
+
+    if amount > sender.balance:
+        return {"error": "Not enough balance"}
+
+    if not sender or not receiver:
+        return {"error": "Account not found"}
+    
+    sender.balance -= amount
+    receiver.balance += amount
+    db.commit()
+    return {"sender_id": sender.id, "sender_balance": sender.balance, "receiver_id": receiver.id, "receiver_balance": receiver.balance, "amount_sent": amount}
